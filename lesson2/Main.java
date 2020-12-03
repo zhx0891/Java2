6bc2376df15ec75c74b7f712684015a2d155ecbb
zhukovv <northbug@yandex.ru>
2020-12-03 22:26:15 +0500

    второй урок  не доделан

--- lesson2/Main.java
+++ lesson2/Main.java
@@ -11,40 +11,45 @@ public class Main {
             x++;
             for (int j = 0; j < arr[0].length; j++) {
                 arr[i][j] = ""+(int)(Math.random()*10);
-                int c=(int)(Math.random()*4);
-                arr[c][c] = "O";
+//                int c=(int)(Math.random()*4);
+//                arr[c][c] = "O";
                 System.out.print(arr[i][j]+" ");
             }
         }
         System.out.println();
     }
-    static void testArr(String[][] e)  {
+    static void testArr(String[][] e)  throws MyArrayDataException{
 
-        if (e.length == 4 & e[0].length == 4) {
+        if (e.length != 4 || e[0].length != 4) {
+            throw new MyArraySizeException();
+        }
 
-            int sum = 0;
+            int sum =0;
+            int x=0;
             for (int i = 0; i < e.length; i++) {
                 for (int j = 0; j < e[0].length; j++) {
-                    int x=0;
-                    try {
-                         x = Integer.parseInt(e[i][j]);
-                    }catch (Exception h){
-                        System.out.println("не верный тип элемента массива");
-                    }
+                    x = Integer.parseInt(e[i][j]);
                     sum +=x;
                 }
+
+                }System.out.println("Сумма массива = "+sum);
+
             }
-            System.out.println("Сумма массива = "+sum);
-        } else {
-            System.out.println("несоразмерный массив");
 
-
-        }
-    }
-
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         createArr(arr1);
-        testArr(arr1);
+        try {
+            testArr(arr1);
+        } catch (MyArrayDataException exc) {
+            System.err.println(exc);
+
+        }catch (MyArraySizeException exc){
+
+        }finally {
+            System.out.println("FINAL");
+        }
+
+
     }
 
 
---
+++ lesson2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package lesson2;
+
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(){
+        super("неверный тип переданного значения массива");
+    }
+}
