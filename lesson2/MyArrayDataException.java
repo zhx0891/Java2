0f7418ae8b441e3ffa89d1ed3b1e70a5a3a329a6
zhukovv <northbug@yandex.ru>
2020-12-07 21:39:52 +0500

    !!!!

--- lesson2/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package lesson2;
-
-public class MyArrayDataException extends NumberFormatException{
-public MyArrayDataException() {
-        super("неверный тип переданного значения массива");
-    }
-}
--- lesson2/MyArraySizeException.java
+++
@@ -1,9 +0,0 @@
-package lesson2;
-
-public class MyArraySizeException extends ArrayIndexOutOfBoundsException{
-public MyArraySizeException() {
-    super("Размер массива отличается от требуемого");
-
-
-}
-}
