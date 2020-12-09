bd25c7e669cc17663518fdd233575029f3fe4524
zhukovv <northbug@yandex.ru>
2020-12-09 20:35:35 +0500

    первое задание

--- lesson3/Main.java
+++ lesson3/Main.java
@@ -6,48 +6,32 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        HashMap<String, String> hm = new HashMap<>();
-//
-//        hm.put("Russia", "Moscow");
-//        hm.put("France", "Paris");
-//        hm.put("Germany", "Berlin");
-//        hm.put("Norway", "Oslo");
-//
-//        for (Map.Entry<String, String> o : hm.entrySet()) {
-//            System.out.println(o.getKey() + ": " + o.getValue());
-//        }
-//        hm.put("Germany", "Berlin2");
-//        System.out.println("New Germany Entry: " + hm.get("Germany"));
-//        List<String> list = new ArrayList<>();
-//        Iterator<String> iter = list.iterator();
-//    }
 
         List<String> list = new ArrayList<>(Arrays.asList("Иванов", "Соколов", "Михайлов", "Степанов", "\n" +
-                "Павлов", "Лебедев", "Козлов", "Степанов", "Михайлов", "Новиков","Кузнецов","Попов", "Алексеев", "Иванов", "Соколов", "Михайлов", "Степанов", "\n" +
-                "Павлов", "Лебедев", "Козлов", "Степанов", "Михайлов", "Новиков","Кузнецов","Попов", "Алексеев"));
+                "Павлов", "Лебедев", "Козлов", "Степанов", "Михайлов", "Новиков", "Кузнецов", "Попов", "Алексеев", "Иванов", "Соколов", "Михайлов", "Степанов", "\n" +
+                "Павлов", "Лебедев", "Козлов", "Степанов", "Михайлов", "Новиков", "Кузнецов", "Попов", "Алексеев"));
         Set<String> set = new HashSet<>(list);
         System.out.println(set);
-        List<String> list2 = new ArrayList<>(Collection set);
-        counterOfDuplicat(list);
+        List<String> list2 = new ArrayList(set);
 
-
-    }
-    public static void counterOfDuplicat(List e){
-//        Iterator<String> iter = e.iterator();
-//        int counter =0;
-        for (int i = 0; i < e.size(); i++) {
-            System.out.println(e.get(i));
-
+        for (String val : list2) {
+        counterOfDuplicat(list, val);
         }
-
-//            while (iter.hasNext()) {
-//            String str= iter.next();
-//            if (str.equals(val) {
-//                counter++;
-//                iter.remove();
-//        }
+    }
+    public static void counterOfDuplicat(List e, String val){
+        int count =0;
+        Iterator<String> iter = e.iterator();
+            while (iter.hasNext()) {
+            String str= iter.next();
+            if (str.equals(val)) {
+                count++;
+                iter.remove();
+            }
 
     }
+        System.out.println(val +" количество  дубликатов: "+ count);
+
+}
 
 }
 
